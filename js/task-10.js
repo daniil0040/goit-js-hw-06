2f5c270435e6800f17f079b51a41a3c47da72b28
daniil0040 <danyapolishchuk777@gmail.com>
2023-08-05 01:17:12 -0500

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -16,17 +16,17 @@ elements.createBtn.addEventListener("click", hendleClickAdd)
 elements.destroyBtn.addEventListener("click", hendleClickRemove)
 
 function hendleClickRemove() {
-  const box = document.querySelector(".js-box")
-  elements.boxes.remove()
+  elements.boxes.innerHTML = ""
+  size = 20
 }
 
 function hendleClickAdd() {
   createBoxes(elements.input.value)
 }
 
-const allDivs = []
 let size = 20
 function createBoxes(amount) {
+  const allDivs = []
   for (let i = 0; i < amount; i += 1) {
     size += 10;
      if (amount >= 1 && amount <= 100) {
